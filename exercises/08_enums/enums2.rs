4551d580023bfe81b25577d95e946f505354f498
Marchello_a <4471190@gmail.com>
2026-05-04 21:48:19 +0300

    solve structs and enums exercises

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -7,11 +7,32 @@ struct Point {
 #[derive(Debug)]
 enum Message {
     // TODO: Define the different variants used below.
+    Resize { width: u32, height: u32 },
+    Move(Point),
+    Echo(String),
+    ChangeColor (u32, u32, u32),
+    Quit
 }
 
 impl Message {
     fn call(&self) {
-        println!("{self:?}");
+        match self {
+            Message::Resize { width, height } => {
+                println!("Resizing to {}x{}", width, height);
+            }
+            Message::Move(point) => {
+                println!("Moving to x={}, y={}", point.x, point.y);
+            }
+            Message::Echo(text) => {
+                println!("Echo: {}", text);
+            }
+            Message::ChangeColor(r, g, b) => {
+                println!("Changing color to RGB({}, {}, {})", r, g, b);
+            }
+            Message::Quit => {
+                println!("Quitting...");
+            }
+        }
     }
 }
 
